1161a095043109618999f88714ae5d98fdb39a5e
Posoroko <eric@posoroko.com>
2023-06-01 12:25:12 +0200

    console buttons

--- components/Board/BoardAbout/BoardAboutMain.vue
+++ components/Board/BoardAbout/BoardAboutMain.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="aboutContainer absoluteFull centered pad5">
-        <div class="aboutBox relative flex column justifyBetween pad10">
-            <h2>BASTARD</h2>
+    <div class="aboutContainer absoluteFull centered">
+        <div class="aboutBox relative flex column gap20 pad20">
+            <h2 class=" aboutTitle">BASTARD</h2>
 
-            <p>
+            <p class="">
                 Bastard est un spectacle en court de création.  Krismenn travaille en ce moment à construire ce qui sera une exploration de ses souvenirs d'enfants.  Ici, il dépose des idées, des inspirations, des visions.  Toutes ces notes participent au développement du spectacle en devenir. En résidence depuis un an à l'hépad de Tréguier, il y découvre petit à petit ce que sera Bastard.  
             </p>
 
@@ -59,15 +59,22 @@ const closeBoard = () => {
 </script>
 
 <style scoped>
+.aboutTitle {
+    padding: 10px 50px;
+}
 .aboutContainer{
     background-color: rgba(0, 0, 0, 0.829);
+    overflow: scroll;
 }
 
 .aboutBox {
     width: min(800px, 100%);
-    min-height: min(500px, 100%);
+    /* min-height: min(500px, 100%); */
     background-color: var(--color-a);
     border-radius: 10px;
+}
+.aboutBox p {
+    font-size: 15px;
 }
 .externalLinkBox div{
     flex-grow: 1;
